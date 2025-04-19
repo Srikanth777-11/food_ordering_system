095672ee93158aee6214edae2d9f5168036b773e
Srikanth777-11 <srikanthmuppalla17@gmail.com>
2025-04-19 13:10:53 +0530

    configchanges

--- src/main/java/com/foodorder/config_server/ConfigServerApplication.java
+++ src/main/java/com/foodorder/config_server/ConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
